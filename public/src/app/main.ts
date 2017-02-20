98361378f351895b1abf90f86b79f5e0b702d934
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-02-20 09:09:52 -0300

    enable production mode

--- public/src/app/main.ts
+++ public/src/app/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { enableProdMode } from '@angular/core';
+
 import { AppModule } from './app.module';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);

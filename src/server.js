d6a37a02a55e183d110566f58c587a25d291f57b
rghossi <rafael.ghossi@gmail.com>
2017-02-23 11:54:58 -0300

    added react and webpack

--- src/server.js
+++ src/server.js
@@ -2,17 +2,22 @@ import http from 'http';
 import express from 'express';
 import path from 'path';
 import apiRoutes from './apiRoutes';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './components/App';
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('dist'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/api', apiRoutes);
 app.get('/', function (req, res) {
-  res.render('index');
+  const appString = renderToString(<App />);
+  res.render('index', { markup: appString });
 });
 
 server.listen(PORT, function() {

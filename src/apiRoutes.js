b4203d87e1067991e89862365a8c426564462b4d
rghossi <rafael.ghossi@gmail.com>
2017-02-23 10:42:43 -0300

    initial server configuration

---
+++ src/apiRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = new Router();
+
+//TODO: add routes
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import http from 'http';
+import express from 'express';
+import path from 'path';
+import apiRoutes from './apiRoutes';
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/api', apiRoutes);
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+server.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}...`);
+});

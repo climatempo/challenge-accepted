76b7a5ae40e1b7e120ab8771210927486e39566f
Willian Oki <willian.oki@gmail.com>
2017-09-05 00:35:28 -0300

    no need for viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { WeatherService } from './climatempo/weather/weather.service';
@@ -10,7 +10,6 @@ import { WeatherService } from './climatempo/weather/weather.service';
 })
 export class AppComponent implements OnInit {
   locations: string[];
-  @ViewChild('searchInput') searchInput;
 
   constructor(private router: Router, private weatherService: WeatherService) { }
 

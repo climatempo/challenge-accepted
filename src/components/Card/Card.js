1789f46f144ebabe267109c0b700794bb57b2ad3
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-04-08 11:25:49 -0300

    Ajustes no css e na responsividade

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -27,33 +27,33 @@ class Card extends Component {
                                 <h5 className="card-title">{newDate}</h5>
                                 <p className="small">{weather.text}</p>
                             </div>
-                            <div className="card-header background-card">
+                            <div className="card-header background-card pt-0 pb-0">
                                 <div className="row row-card">
                                     <div className="col-2">
-                                        <img src={img4} />
+                                        <img className="item" src={img4} />
                                     </div>
                                     <div className="col-4">
-                                        <h4 className="text-primary">{weather.temperature.max}°C</h4>
+                                        <h4 className="text-primary text-center">{weather.temperature.max}°C</h4>
                                     </div>
                                     <div className="col-2">
-                                        <img src={img1} />
+                                        <img className="item" src={img1} />
                                     </div>
                                     <div className="col-4">
-                                        <h4 className="text-danger">{weather.temperature.min}°C</h4>
+                                        <h4 className="text-danger text-center">{weather.temperature.min}°C</h4>
                                     </div>
                                 </div>
-                                <div className="row row-card">
+                                <div className="row row-card ">
                                     <div className="col-2">
-                                        <img src={img3}  />
+                                        <img className="item" src={img3}  />
                                     </div>
                                     <div className="col-4">
-                                        <h4>{weather.temperature.min}mm</h4>
+                                        <h4  className="text-center">{weather.temperature.min}mm</h4>
                                     </div>
                                     <div className="col-2">
-                                        <img src={img2} />
+                                        <img className="item" src={img2} />
                                     </div>
                                     <div className="col-4">
-                                        <h4>{weather.temperature.min}%</h4>
+                                        <h4  className="text-center">{weather.temperature.min}%</h4>
                                     </div>
                                 </div>
                             </div>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import icone from '../../images/icons/search.png';
-import Card from '../Card/Card';
 import { searchWeather } from '../../actions/actions'
 
 
@@ -16,12 +15,9 @@ class Search extends Component {
     this.searchWeather = this.searchWeather.bind(this);
   }
 
-
-
   searchWeather = e => {
     e.preventDefault();
     this.props.searchWeather(this.state.locale);
-    // axios.get(`http://localhost:3001/api/weathers/${this.state.locale}`).then(res => this.setState({ weathers: res.data }));
   }
 
   render() {
@@ -30,14 +26,14 @@ class Search extends Component {
       <div className="container-fluid no-pading">
         <form className="row" onSubmit={e => this.searchWeather(e)}>
           <div className="input-group">
-            <input type="text" className="form-control no-border" placeholder="Digite o local" value={this.state.locale} onChange={e => this.setState({ locale: e.target.value })} aria-label="Digite o local" aria-describedby="basic-addon2" />
+            <input type="text" className="form-control no-border font" placeholder="Digite o local" value={this.state.locale} onChange={e => this.setState({ locale: e.target.value })} aria-label="Digite o local" aria-describedby="basic-addon2" />
             <div className="input-group-append">
               <button className="btn btn-outline-secondary no-border button-search" type="button" onClick={this.searchWeather}><img className="img-fluid" src={icone} /></button>
             </div>
           </div>
         </form>
         {weathers.locale ? <div>
-          <h4 className="mt-3 mb-3">Previsão para {weathers.locale.name} - {weathers.locale.state}</h4>
+          <h4 className="text-title">Previsão para {weathers.locale.name} - {weathers.locale.state}</h4>
         </div> : null}
       </div>
     );

d8d8199ca9060a47fa21000c52d2497b542eab80
santosjn <jeansjcsp@gmail.com>
2018-05-05 20:38:06 -0300

    Adding bootstrap. Rendering main template for data.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var weatherRouter = require('./routes/weather');
 
 var app = express();
 
@@ -19,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes Description
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/weather', weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

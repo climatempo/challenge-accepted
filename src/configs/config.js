338b707418600b17ced4f4e4092d5f44c697c121
jasondavindev <jasondavin.dev@gmail.com>
2018-12-15 20:00:21 -0200

    Fix Commit bug

--- src/configs/config.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-const GlobalConfig = {
-    FILE_LOCALES: path.join('base', 'locales.json'),
-    FILE_WEATHER: path.join('base', 'weather.json'),
-    PORT: 3000
-}
-
-export default GlobalConfig;
--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-class IndexController {
-    get(req, res) {
-        res.render('index', { title: 'API v1.0' });
-    }
-}
-
-export default IndexController;

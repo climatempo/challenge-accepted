de8eab2743ad9a7d10ed5205739a364cac460a4e
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-01-17 17:21:42 -0200

    feat: CardList agora processa os dados climaticos para gerar Cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,12 +3,21 @@ import Card from "./Card";
 
 class CardList extends Component {
   render() {
+    // Quebra o retorno JSON em partes para utilizar os dados
     const weatherJSON = this.props.weather;
+    var weatherData = []
+
+    if(weatherJSON) {
+      var localeName = weatherJSON.locale.name
+      var localeState = weatherJSON.locale.state
+      weatherData = weatherJSON.weather
+    }
     return (
-      <div>
+      <div className="tc h-100" style={{backgroundColor: "#f0f0f0"}}>
+        <h2>Previsão para {localeName} - {localeState}</h2>
         {
-          weatherJSON.map(obj => {
-            return <Card key={obj.locale.id} name={obj.locale.name} />
+          weatherData.map(weather => {
+            return <Card key={weather.date} name={localeName} state={localeState} date={weather.date} text={weather.text} temperature={weather.temperature} rain={weather.rain} />
           })
         }
       </div>

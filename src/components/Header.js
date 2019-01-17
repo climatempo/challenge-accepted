282de4a4b11fbb143979db8a63a3c30fcfe85285
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-01-17 17:23:28 -0200

    style: Prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import logo from "../images/logo-white.png";
-import './Header.css';
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
       <div className="tc bg-dark-blue w-100" style={{ height: "8vh" }}>
-        <span className="dib h-100 v-mid"></span><img className="v-mid" src={logo} alt="logo-white" width="30%" />
+        <span className="dib h-100 v-mid" />
+        <img className="v-mid" src={logo} alt="logo-white" width="30%" />
       </div>
     );
   }

180c613b9b1b350e02a0ebb07754282048d8a175
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-01-17 12:36:07 -0200

    feat: Criacao do componente CardList
    Este componente ira agrupar os Cards

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Card from "./Card";
+
+class CardList extends Component {
+  render() {
+    return (
+      <div>
+        <Card />
+      </div>
+    );
+  }
+}
+
+export default CardList;

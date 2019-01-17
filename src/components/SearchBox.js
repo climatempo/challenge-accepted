9eef6c73dafeb2acc6fbc733d17f12f7ca713d59
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-01-17 17:23:16 -0200

    feat: Adicionando imagem ao lado da barra de pesquisa
    Cores e css foram modificados

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,14 +4,23 @@ import searchIcon from "../images/icons/search.png";
 // Exemplo de como um objeto seria sem o uso de Component
 const SearchBox = ({ onSearchEnter }) => {
   return (
-    <div className="dif flex-row p2 tc content-center">
+    <div
+      className="dif flex-row p2 tc content-center"
+      style={{ backgroundColor: "white" }}
+    >
       <input
         className="pa3 ba bw0 w-80"
         type="search"
         placeholder="Digite o nome da cidade"
         onKeyPress={onSearchEnter}
       />
-      <span className="dib h-100 v-mid"></span><img className="v-mid pa1" src={searchIcon} alt="Search" style={{width: "30px"}} />
+      <span className="dib h-100 v-mid" />
+      <img
+        className="v-mid pa1"
+        src={searchIcon}
+        alt="Search"
+        style={{ width: "30px" }}
+      />
     </div>
   );
 };

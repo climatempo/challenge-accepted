96c8d6e098ac9f049de8b4df92bffeba1e9b4303
Ana Lima <analimazn@gmail.com>
2019-01-26 21:32:44 -0200

    Retirar espaços e acentos dos dados recebidos pela rota

--- routes/locales.js
+++ routes/locales.js
@@ -1,6 +1,11 @@
 'use-strict';
 
 const modelLocale = require('../base/locales.json');
+const removeAccents = require('remove-accents');
+
+function parseText(text) {
+	return removeAccents(String((text).replace(" ", "")).toLowerCase());
+}
 
 async function getLocaleByName(server) {
 	try {
@@ -8,11 +13,10 @@ async function getLocaleByName(server) {
 			method: 'GET',
 			path: '/locales/{name}',
 			handler: (req, head) => {
-				let name = req.params.name;
+				let name = parseText(req.params.name);
 				return modelLocale.filter(data => {
-					let parseNameReceived = String(name).toLowerCase();
-					let parseNameData = String(data.name).toLowerCase();
-					return parseNameData == parseNameReceived ? data.id : null;
+					let locale = parseText(data.name);
+					return locale == name ? data.id : null;
 				})
 			}
 		});

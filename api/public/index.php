92b725ae2e39a99e98cca321e1d0a9abd5e114a2
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-02-23 23:48:51 -0300

    CORS allowed in the API

--- api/public/index.php
+++ api/public/index.php
@@ -13,6 +13,16 @@ $app = new class () extends App
     }
 };
 
+// Configuring the CORS, so that the React SPA can access the API
+$app->add(new Tuupola\Middleware\CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => [],
+    "headers.expose" => [],
+    "credentials" => false,
+    "cache" => 0,
+]));
+
 // Routes definition
 require __DIR__ . "/../src/config/routes.php";
 

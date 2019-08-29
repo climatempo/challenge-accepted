3c8f65c0a2de07d509fc6ddd7378c9ab75081357
Mauricio Ribeiro <mauricio@justto.com.br>
2019-08-29 20:10:15 -0300

    Cria projeto laravel

---
+++ api/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('locales')->group(function () {
+
+    Route::get('', 'LocaleController@index');
+
+});

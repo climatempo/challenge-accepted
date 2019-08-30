39c69141e719c77df2dac15f5c46c45d7effc457
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-30 02:07:14 -0300

    Added check if name exists

--- backend-nodejs-express/src/services/region-search/controller.js
+++ backend-nodejs-express/src/services/region-search/controller.js
@@ -3,25 +3,27 @@ import { esGet } from '../../utils/elasticsearch';
 import { getLocalesByName } from './model';
 
 export default class Controller {
-  static async index({ query }, res) {
+  static async index({ query: { name } }, res) {
     let locales = [];
 
-    const payload = {
-      query: {
-        query_string : {
-          query: `*${query.name}*`,
-          fields: ['name', 'key'],
+    if (name) {
+      const payload = {
+        query: {
+          query_string : {
+            query: `*${name}*`,
+            fields: ['name', 'key'],
+          },
         },
-      },
-    };
+      };
 
-    try {
-      await esGet('locales', payload).then(({ body: { hits: { hits } } }) => {
-        locales = hits.map(({ _source: locale }) => ({ id: locale.id, name: locale.name }));
-      });
-    } catch (err) {
-      locales = await getLocalesByName(query.name);
-      global.console.error(err);
+      try {
+        await esGet('locales', payload).then(({ body: { hits: { hits } } }) => {
+          locales = hits.map(({ _source: locale }) => ({ id: locale.id, name: locale.name }));
+        });
+      } catch (err) {
+        locales = await getLocalesByName(name);
+        global.console.error(err);
+      }
     }
 
     res.send({ locales });

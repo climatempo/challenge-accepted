f64b623c048e20f9a7cc00ffbda55381fbe7c139
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-30 00:15:12 -0300

    Validated query parameters

--- backend-nodejs-express/src/services/region-search/index.js
+++ backend-nodejs-express/src/services/region-search/index.js
@@ -1,8 +1,12 @@
+import { buildCheckFunction } from 'express-validator';
+
 import Controller from './controller';
+
+const check = buildCheckFunction(['query']);
 
 export default (app) => {
   const BASE = '/api';
-  app.get(`${BASE}/v1/regions`, Controller.index);
+  app.get(`${BASE}/v1/regions`, [check('name').exists()], Controller.index);
 };
 
 export const regionSearchController = Controller;
--- backend-nodejs-express/src/services/weather/index.js
+++ backend-nodejs-express/src/services/weather/index.js
@@ -1,8 +1,12 @@
+import { buildCheckFunction } from 'express-validator';
+
 import Controller from './controller';
+
+const check = buildCheckFunction(['query']);
 
 export default (app) => {
   const base = '/api';
-  app.get(`${base}/v1/weather`, Controller.index);
+  app.get(`${base}/v1/weather`, [check('localeId').exists()], Controller.index);
 };
 
 export const weatherController = Controller;

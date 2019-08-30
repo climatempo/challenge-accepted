b3f63476361aded8165e1ce55057f0ed90261252
Mauricio Ribeiro <mauricio@justto.com.br>
2019-08-30 19:07:16 -0300

    Cria aplicação

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <HelloWorld />
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+import HelloWorld from '../components/HelloWorld';
 
-@Component({
+export default {
   components: {
     HelloWorld,
   },
-})
-export default class Home extends Vue {}
+};
 </script>

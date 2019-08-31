fdf206a8dcd550a3b7c5fd48ffa153f4442a7ca7
Mauricio Ribeiro <mauricio@justto.com.br>
2019-08-30 23:23:25 -0300

    Adiciona titulo no router

--- web/src/router.js
+++ web/src/router.js
@@ -3,18 +3,22 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+      {
+          path: "/",
+          name: "index",
+          meta: {
+              title: "Climatempo | Previsões por Localização"
+          }
+      }
   ]
 });
+
+router.afterEach(function (to) {
+    document.title = to.meta.title || 'Climatempo';
+});
+
+export default router;

8b4421ecde37ac83175347392c4baf032335ddf6
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-08-30 22:05:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
 /* eslint-disable arrow-parens */
-const cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const next = require('next');
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const next = require("next");
 
-const api = require('./API');
+const api = require("./API");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -17,8 +17,8 @@ app.prepare().then(() => {
   server.use(cors()); // { origin: "https://fretesbrasil.herokuapp.com" }
   server.use(bodyParser.urlencoded({ extended: false }));
 
-  server.use('/api', api);
-  server.get('*', (req, res) => handle(req, res));
+  server.use("/api", api);
+  server.get("*", (req, res) => handle(req, res));
   server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);

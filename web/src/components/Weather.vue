a8ff19d6e2e9f700ad18a8d1e3c8343c66c536ca
Mauricio Ribeiro <mauricio@justto.com.br>
2019-08-30 23:02:11 -0300

    Cria cards de previsões

--- web/src/components/Weather.vue
+++ web/src/components/Weather.vue
@@ -3,22 +3,55 @@
     <v-card class="mx-auto" outlined>
       <v-list-item three-line>
         <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
-          <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+            <v-list-item-title class="headline">27/08/2019</v-list-item-title>
+            <v-list-item-subtitle>Sol com muitas nuvens durante o dia. Períodos de nublado, com chuva a qualquer hora.</v-list-item-subtitle>
         </v-list-item-content>
-
-        <v-list-item-avatar
-                tile
-                size="80"
-                color="grey"
-        ></v-list-item-avatar>
       </v-list-item>
-
-      <v-card-actions>
-        <v-btn text>Button</v-btn>
-        <v-btn text>Button</v-btn>
-      </v-card-actions>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-row>
+          <v-col class="temperature-min col-sm-3 col-6">
+            <v-card dark color="teal darken-1">
+              <v-card-text>
+                <v-layout row class="justify-space-between px-2">
+                  <v-icon>fas fa-arrow-down</v-icon>
+                  <span class="title">19°</span>
+                </v-layout>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="temperature-max col-sm-3 col-6">
+            <v-card dark color="red darken-2">
+              <v-card-text>
+                <v-layout row class="justify-space-between px-2">
+                  <v-icon>fas fa-arrow-up</v-icon>
+                  <span class="title">27°</span>
+                </v-layout>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="probability col-sm-3 col-6">
+            <v-card dark color="blue darken-1">
+              <v-card-text>
+                <v-layout row class="justify-space-between px-2">
+                  <v-icon>fas fa-tint</v-icon>
+                  <span class="title">77mm</span>
+                </v-layout>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="precipitation col-sm-3 col-6">
+            <v-card dark color="purple darken-3">
+              <v-card-text>
+                <v-layout row class="justify-space-between px-2">
+                  <v-icon>fas fa-umbrella</v-icon>
+                  <span class="title">25%</span>
+                </v-layout>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>

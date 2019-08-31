e7d90efb2405d872d050322f292d428871e3db00
Matheus Pavanetti <matheuspavanetti@gmail.com>
2019-08-31 15:43:49 -0300

    Foi adicionado uma rota padrão para mapear o controller Previsao_Controller, foi corrigido o nome da classe no controller Previsao_Controller.

--- application/controllers/Previsao_Controller.php
+++ application/controllers/Previsao_Controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Previsao extends CI_Controller {
+class Previsao_Controller extends CI_Controller {
 
 	public function index()
 	{

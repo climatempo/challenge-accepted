c3d3534fcc0e5c97a6d3189c537d5f6adc50ded6
Léo Castro <leonardo_carvalho@outlook.com>
2019-09-01 20:24:15 -0300

    Implemented get weather details by region with redis

---
+++ backend-php7-symfony/src/Controller/WeatherController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+use Psr\Log\LoggerInterface;
+
+use App\Service\RedisCache;
+
+use App\Model\Climate\Weather;
+
+class WeatherController extends AbstractController
+{
+    private $redisCache;
+    private $logger;
+
+    public function __construct(RedisCache $redisCache, LoggerInterface $logger)
+    {
+        $this->redisCache = $redisCache;
+        $this->logger = $logger;
+    }
+
+    public function index(Request $request)
+    {
+        $localeId = (int) $request->query->get('localeId');
+
+        $cacheKey = ':weather:' . strval($localeId);
+
+        $resp = [];
+        
+        try {
+            if ($this->redisCache->keyExists($cacheKey)) {
+                $resp = $this->redisCache->get($cacheKey);
+            } else {
+                $data = Weather::getWeatherByLocaleId($localeId);
+                $this->redisCache->set($cacheKey, $data, 60); // 60 >> seconds
+                $resp = $data;
+            }
+        } catch (Exceptiom $e) {
+            $logger->error($e->getMessage());
+        }
+
+        return $this->json(['weather' => $resp]);
+    }
+    
+}
---
+++ backend-php7-symfony/src/Model/Climate/Weather.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model\Climate;
+
+use App\Service\RedisCache;
+
+class Weather
+{
+    private static $weathers = [];
+
+    private static function getWeatherData()
+    {
+        if (empty(self::$weathers)) {
+            try {
+                self::$weathers = json_decode(file_get_contents(__DIR__ . '/weather.json'));
+            } catch(Exception $err) {
+                // TODO: add logger
+            }
+        }
+
+        return self::$weathers;
+    }
+
+    public static function getWeatherByLocaleId(int $localeId)
+    {
+        $result = array_filter(self::getWeatherData(), function($weather) use ($localeId) {
+            return $weather->locale->id === $localeId;
+        });
+
+        return $result ? $result['1'] : [];
+    }
+
+}

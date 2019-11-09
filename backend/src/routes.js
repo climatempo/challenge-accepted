5eb51e0665ecfbc54344beca88b063bcc506cbeb
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-09 16:15:31 -0300

    feat: add routes weather and locales.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,3 +1,10 @@
 const routes = require('express').Router();
 
+const weatherController = require('./app/Controllers/weatherController');
+const localesControllerv = require('./app/Controllers/localesController');
+
+routes.get('/weather/:id', weatherController.show);
+routes.get('/locales', localesControllerv.index);
+routes.get('/locales/city', localesControllerv.show);
+
 module.exports = routes;

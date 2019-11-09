4d2a1d48ce40c6ac36d56e72320f972ce9b37199
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-09 20:02:00 -0300

    feat: add entier file reactJs.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './pages/Main';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <Main />
+      <GlobalStyle />
+    </>  
+  );
+}
+
+export default App;

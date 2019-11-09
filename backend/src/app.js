f45d36ba0845b1136d5c57baa77e51acf3d75e1b
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-09 15:46:24 -0300

    feat: add cors dependencies to be able to access external api.

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ require('dotenv').config({
 });
 
 const express = require('express');
+const cors = require('cors');
 
 class AppController {
   constructor() {
@@ -13,6 +14,7 @@ class AppController {
 
   middlewares() {
     this.express.use(express.json());
+    this.express.use(cors());
   }
 
   routes() {

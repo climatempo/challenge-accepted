f3c8d5f43dcb8d6ec8f71565350a5db8f11b8443
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-10 01:48:14 -0300

    style: file format adjustment.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env',
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
 const express = require('express');

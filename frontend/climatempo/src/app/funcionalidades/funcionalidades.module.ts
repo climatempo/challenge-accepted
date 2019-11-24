619d80935753e0cac97f9be23b93525e5e76ffcb
gabriel fullstack <gabriel.felicio@squadra.com.br>
2019-11-24 13:49:14 -0300

    Ajustes finais

--- frontend/climatempo/src/app/funcionalidades/funcionalidades.module.ts
+++ frontend/climatempo/src/app/funcionalidades/funcionalidades.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from './home/home.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+
 import { HomeComponent } from './home/home.component';
 import { PrevisaoTempoComponent } from './previsao-tempo/previsao-tempo.component';
 import { ListPrevisaoTempoComponent } from '../funcionalidades/previsao-tempo/list-previsao-tempo/list-previsao-tempo.component';
 import { FormPrevisaoTempoComponent } from '../funcionalidades/previsao-tempo/form-previsao-tempo/form-previsao-tempo.component';
 import { FuncionalidadesRoutingModule } from './funcionalidades-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatRippleModule
-} from '@angular/material';
+
+import { environment } from "../../environments/environment";
 
 const modules = [
   MatButtonModule,
@@ -33,7 +31,10 @@ const modules = [
     FlexLayoutModule,
     HomeModule,
     modules,
-    MatCardModule
+    MatCardModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.apiGoogle
+    })
   ],
   exports: [
     modules
--- frontend/climatempo/src/environments/environment.ts
+++ frontend/climatempo/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:8000'
+  url: 'http://localhost:8000',
+  apiGoogle: 'AIzaSyAm1jTsYmD7Nf26rWYlxZAYJDSNkvUR-OM'
 };

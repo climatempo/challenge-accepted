f272ba9591c128b273549d346cf17f28191f8dd5
Rafael França <rafaelfrancam@gmail.com>
2020-01-27 21:06:46 -0300

    Change morgan config on app.js

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('immediate', {
-  stream: fs.createWriteStream(path.join(__dirname, 'access.log'))
-}));
+app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

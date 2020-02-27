a57c83f75197daf8f9f6d85feb27f004809d2097
Vinicius <cardoso_cpv@hotmail.com>
2020-02-27 04:20:46 -0300

    filtro adicionado no endpoint de locales

--- server/api/locales.js
+++ server/api/locales.js
@@ -1,9 +1,17 @@
-const data = require('./base/locales.json')
+let locales = require('./base/locales.json')
 const app = require('express')()
 
-app.get('/', (req, res) => res.send({
-  success: true,
-  data
-}))
+app.get('/', (req, res) => {
+  // Filtra as cidades baseado no paremetro q
+  if (req.query.q) {
+    locales = locales.filter((locale) => (
+      (new RegExp(req.query.q.toLowerCase())).test(locale.name.toLowerCase())
+    ))
+  }
+  return res.send({
+    success: true,
+    locales
+  })
+})
 
 module.exports = app

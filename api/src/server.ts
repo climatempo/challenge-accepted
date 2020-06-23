089a43f382597c9c8f1f95d0467f4e68d4f5409a
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-06-23 19:25:25 -0300

    Estrutura des pastas e organização de arquivos do front

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333!");
+app.listen(3001, () => {
+  console.log("Server started on port 3001!");
 });
---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>Opsss</p>
+    </div>
+  );
+}
+
+export default App;

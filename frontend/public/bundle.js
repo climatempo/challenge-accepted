e6f9a65efb9a2935eec41550afb3e861159e1be4
Eduardo Nunes <nunes_eduardo@rocketmail.com>
2020-12-30 01:00:27 +0000

    estrutura aplicação react

---
+++ frontend/public/bundle.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+eval("var soma = function soma(a, b) {\n  return a + b;\n};\n\nconsole.log(soma(2, 3));\n\n//# sourceURL=webpack://frontend/./src/index.js?");
+/******/ })()
+;

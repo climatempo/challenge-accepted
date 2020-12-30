14a4539da40eb94c712404c391831d60deaa267d
Eduardo Nunes <nunes_eduardo@rocketmail.com>
2020-12-30 01:16:10 +0000

    habilitar estilização com css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './App.css'
+
 function App() {
   return (
     <h1>Hello World</h1>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
       }
     ]
   }

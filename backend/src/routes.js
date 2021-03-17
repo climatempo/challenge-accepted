ac0701ae8be7a79d4025f06678da982a621680ae
Vinicius <vinicius.assis@acct.global>
2021-03-17 15:35:14 -0300

    Create routes

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const locationService = require('./factories/locationFactory').locationInstance()
+const weatherService = require('./factories/weatherFactory').weatherInstance()
+
+module.exports = {
+  '/location:get': async (request, response) => {
+    const { id } = request.queryString
+    const location = await locationService.find(id)
+    response.write(JSON.stringify(location))
+    return response.end()
+  },
+  '/weather:get': async (request, response) => {
+    const { id } = request.queryString
+    const weather = await weatherService.find(id)
+    response.write(JSON.stringify(weather))
+    return response.end()
+  },
+  default: (request, response) => {
+    response.write('{ message: "Page not found" }')
+    response.end()
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 const http = require('http')
+const path = require('path')
 const PORT = 3000
 const DEFAULT_HEADER = { 'Content-Type': 'application/json' }
+const routes = require('./routes')
+
 
 const handler = (request, response) => {
   const { url, method } = request
 
-  if (method !== 'GET') {
-    response.statusCode = 405
-    response.end('Method not allowed.')
-  }  
+  const [first, route, id] = url.split('/')
+  request.queryString = { id: isNaN(id) ? id : Number(id)}
+  
+  const key = `/${route}:${method.toLowerCase()}`
+
+  response.writeHead(200, DEFAULT_HEADER)
+
+  const chosen = routes[key] || routes.default
+  return chosen(request, response)
 }
 
 http.createServer(handler)

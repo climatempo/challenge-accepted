a2cd80f7693836583eab5822097fab5d39781ca4
Vinicius <vinicius.assis@acct.global>
2021-03-21 12:47:07 -0300

    Add fetch location in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Search from "./components/SearchInput";
 import GlobalStyle from "./styles/GlobaStyle";
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Header />
-    <Search />
-    <Content />
-  </>
-);
+const App = () => {
+  const [locations, setLocations] = useState<string>("");
+
+  const fetchLocation = async () => {
+    const url = `http://localhost:8000/location`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setLocations(JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  useEffect(() => {
+    locations && localStorage.setItem("locations", locations);
+  }, [locations]);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Search />
+      <Content />
+    </>
+  );
+};
 
 export default App;
--- frontend/src/components/SearchInput/index.tsx
+++ frontend/src/components/SearchInput/index.tsx
@@ -8,24 +8,14 @@ const Search = () => {
     setInputValue(event.target.value);
   };
 
-  const fetchLocation = async (value: string) => {
-    const url = `http://localhost:8000/location/${value.toLowerCase()}`;
-    if (value) {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      return data;
-    }
-  };
-
   return (
     <SearchWrapper>
       <SearchInput
         onChange={handleChange}
-        onKeyDown={(e) => e.key === "Enter" && fetchLocation(inputValue)}
+        // onKeyDown={(e) => e.key === "Enter" && fetchLocation(inputValue)}
       />
       {inputValue}
-      <SearchButton onClick={() => fetchLocation(inputValue) as any} />
+      <SearchButton />
     </SearchWrapper>
   );
 };

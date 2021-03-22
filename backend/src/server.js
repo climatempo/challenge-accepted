3b82288316ee5ea7c30adfbf7fbffa2861b55e97
Vinicius <vinicius.assis@acct.global>
2021-03-22 20:48:50 -0300

    Create config file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,6 @@
 const http = require("http");
-const PORT = 8000;
-const DEFAULT_HEADER = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "Authorization, Cache-Control, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-  "Access-Control-Max-Age": 2592000,
-};
 const routes = require("./routes");
+const { PORT, DEFAULT_HEADER} = require("./config")
 
 const handler = (request, response) => {
   const { url, method } = request;

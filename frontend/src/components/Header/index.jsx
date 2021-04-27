988393a223ecfe2494e4a20c87eee68f11ee7dfb
Talles <talles17.a@hotmail.com>
2021-04-27 17:02:58 -0300

    feat-frontend: add header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import logo from '../../assets/logo-white.png';
+import styles from './header.module.css';
+
 export default function Header() {
   return (
-    <header>
-      <div>CLIMATEMPOLOGO</div>
+    <header className={styles.container}>
+      <div>
+        <img src={logo} alt="climatempo logo" />
+      </div>
     </header>
   );
 }

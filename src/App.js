f4838d6b9bc1dc36380ec15451ef7f7d3850563a
Talita Pastorini <talitapastorini@gmail.com>
2021-05-04 19:08:32 -0300

    test 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div 
-  className="App"
->
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

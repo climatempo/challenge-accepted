c15b487e25c989da5be3511dff8e1fbfb9fc418a
Talita Pastorini <talitapastorini@gmail.com>
2021-05-04 19:01:24 -0300

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div 
+  className="App"
+>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

f31f2d15c9c59a04864bbcc95689f4dcfa3841da
Talita Pastorini <talitapastorini@gmail.com>
2021-05-11 08:55:00 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

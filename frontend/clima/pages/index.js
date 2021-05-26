a72dad935c71bf8a0862dc690bb8e346252e33e4
elninocavalcanti10 <acn2011@outlook.com>
2021-05-26 10:27:33 -0300

    Criando conversores

--- frontend/clima/pages/index.js
+++ frontend/clima/pages/index.js
@@ -8,6 +8,8 @@ function Home({dados}) {
   const [city, setCity] = useState('');
   const [inputError, setInputError] = useState('');
   const [resultados, setResultados] = useState([]);
+  const [isFahrenheit, setIsFahrenheit] = useState(false);
+  const [isInch, setIsInch] = useState(false);
 
   const handleOnSearch = (string, results) => {
     console.log(string, results, 'string', 'results')
@@ -60,12 +62,42 @@ function Home({dados}) {
       </form>
       {inputError && <span className={styles.span}>{inputError}</span>}
       
-      {console.log(resultados.map(item => item.weather.map(we => new Date(we.date).toLocaleDateString())))}
-      
+      {console.log(resultados, 'ghuighygyhgbyhvvgukvhvh')}
+      {typeof resultados != "undefined" && resultados != null && resultados.length != null && resultados.length > 0
+        ?      
+        <div className={styles.topo}>
+          <h2 className={styles.title}>
+            Previsão para {resultados.map(item => item.locale.name.toString())} - SP
+          </h2>
+          {isInch === false
+            ?
+            <button className={styles.conversorF} onClick={() => {setIsInch(true)}}>
+              Covert para "inch"
+            </button>
+            :
+            <button className={styles.conversorC} onClick={() => {setIsInch(false)}}>
+              Covert para "mm"
+            </button>
+          }
+          {isFahrenheit === false
+            ?
+            <button className={styles.conversorF} onClick={() => {setIsFahrenheit(true)}}>
+              Covert para ºF
+            </button>
+            :
+            <button className={styles.conversorC} onClick={() => {setIsFahrenheit(false)}}>
+              Covert para ºC
+            </button>
+          }
+        </div>
+        :
+        <div className={styles.topo}>
+          <span>Climatempo - Busque a previsão para sua cidade nos próximos dias.</span>
+        </div>
+      }
 
-      <h2 className={styles.title}>
-        Previsão para {resultados.map(item => item.locale.name.toString())} - SP
-      </h2>
+
+      
 
       <ul>
         {resultados.map(item => item.weather.map(we => (
@@ -78,17 +110,44 @@ function Home({dados}) {
               <div className={styles.cardColumnOne}>
                 <div className={styles.cardColumnOneRowOne}>
                   <img src="/icons/upload.png" />
-                  <span className={styles.grau}>{we.temperature.max}°C</span>
+                  {isFahrenheit === false 
+                    ? 
+                      <span className={styles.grau}>
+                        {we.temperature.max}°C
+                      </span>
+                    : 
+                      <span className={styles.grau}>
+                        {((we.temperature.max * 1.8) + 32).toFixed(0)}°F
+                      </span>
+                  }
                 </div>
                 <div className={styles.cardColumnOneRowTwo}>
                   <img src="/icons/raindrop-close-up.png" />
-                  <span className={styles.milimetros}>{we.rain.precipitation}mm</span>
+                  {isInch === false 
+                    ? 
+                      <span className={styles.milimetros}>
+                        {we.rain.precipitation}mm
+                      </span>
+                    : 
+                      <span className={styles.milimetros}>
+                        {(we.rain.precipitation / 25.4).toFixed(2)}inch
+                      </span>
+                  }
                 </div>
               </div>
               <div className={styles.cardColumnTwo}>
                 <div className={styles.cardColumnTwoRowOne}>
                   <img src="/icons/download.png" />
-                  <span className={styles.grau}>{we.temperature.min}°C</span>
+                  {isFahrenheit === false 
+                    ? 
+                      <span className={styles.grau}>
+                        {we.temperature.min}°C
+                      </span>
+                    : 
+                      <span className={styles.grau}>
+                        {((we.temperature.min * 1.8) + 32).toFixed(0)}°F
+                      </span>
+                  }
                 </div>
                 <div className={styles.cardColumnTwoRowTwo}>
                   <img src="/icons/protection.png" />

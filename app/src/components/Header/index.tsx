da9231d4e7097e3a209c40a384d906051cd7f031
~Arthur <~arthurborges@ufu.br>
2022-08-29 16:08:09 -0300

    progress: finishing the header component

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,7 +1,27 @@
-import { Container } from "./styles";
+import * as C from "./styles";
+import WhiteLogo from "../../images/logo-white.png";
+import SearchIcon from "../../images/icons/search.png";
 
 export const Header = () => {
     return (
-        <Container />
+        <C.Container>
+            <C.HeaderContent>
+                <C.Logo
+                    src={WhiteLogo}
+                    alt="Climatempo logo"
+                />
+                <C.Search>
+                    <C.Input
+                        type="search"
+                        placeholder="Busque por uma cidade..."
+                        aria-label="Search field"
+                    />
+                    <C.SearchLogo
+                        src={SearchIcon}
+                        alt="Search icon"
+                    />
+                </C.Search>
+            </C.HeaderContent>
+        </C.Container>
     );
 };
--- app/src/components/Header/styles.ts
+++ app/src/components/Header/styles.ts
@@ -5,3 +5,49 @@ export const Container = styled.header`
     height: 80px;
     background-color: #0080CD;
 `;
+
+export const HeaderContent = styled.div`
+    margin: 0 auto;
+    max-width: 1170px;
+    height: 100%;
+    padding: 0 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Logo = styled.img`
+    width: 120px;
+`;
+
+export const Search = styled.div`
+    flex: .95;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    background-color: #0267A3;
+    border-radius: 5px;
+    padding: 5px 20px;
+`;
+
+export const Input = styled.input`
+    flex: 1;
+    height: 100%;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    color: #FFF;
+
+    ::placeholder {
+        color: inherit;
+        opacity: 50%;
+        font-size: 15px;
+        font-weight: 100;
+    }
+`;
+
+export const SearchLogo = styled.img`
+    height: 55%;
+    filter: invert(100%);
+    opacity: 50%;
+`;

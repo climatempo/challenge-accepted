4fd9d5ae3f910e41cb462c9e0d1665d9f51b2001
phfdonda <phfdonda@gmail.com>
2022-12-04 08:50:01 -0300

    Ajustes finos em estilo - mobile first

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState } from "react"
 import Image from "next/image"
 
 import menu from "public/images/icons/menu.png"
@@ -6,7 +6,13 @@ import close from "public/images/icons/close.png"
 import styles from "styles/components/Menu.module.scss"
 
 export default function Menu(props) {
+  const [isSettingsOpen, setSettingsOpen] = useState(false)
   const { handleTemperature, handleVolume } = props
+
+  function handleOpen() {
+    setSettingsOpen(!isSettingsOpen)
+  }
+
   return (
     <div className={styles.container}>
       <Image
@@ -15,14 +21,16 @@ export default function Menu(props) {
         alt="barras de menu"
         width="30"
         height="30"
+        onClick={handleOpen}
       />
-      <menu className={styles.menu}>
+      <menu className={`${styles.menu} ${isSettingsOpen ? "" : styles.hidden}`}>
         <Image
           className={styles.close}
           src={close}
           alt="barras de menu"
           width="20"
           height="20"
+          onClick={handleOpen}
         />
         <li className={styles.toggle}>
           <label className={styles.label} htmlFor="temperature">

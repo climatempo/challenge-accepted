d3904f7dd545bf55fe669fc8681e2205f2de6501
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-12-28 22:36:32 -0300

    feat: header

--- project/client/src/components/Header/styles.js
+++ project/client/src/components/Header/styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components"
 
 export const Header = styled.header`
 	width: 100%;
-	height: 5rem;
+	height: 6rem;
+	padding: 2rem;
 	background-color: var(--color-blue-200);
 	img {
 		width: 100%;
 		height: 100%;
-		object-fit: cover;
+		object-fit: contain;
 	}
 `

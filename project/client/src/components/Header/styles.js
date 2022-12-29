e13ebea5d6d724d410e73e8299b71fc2eff45297
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-12-28 22:51:11 -0300

    style: header width

--- project/client/src/components/Header/styles.js
+++ project/client/src/components/Header/styles.js
@@ -2,12 +2,12 @@ import styled from "styled-components"
 
 export const Header = styled.header`
 	width: 100%;
-	height: 6rem;
+	height: 5rem;
 	position: fixed;
 	top: 0;
 	left: 0;
 	z-index: 1;
-	padding: 2rem;
+	padding: 1.5rem;
 	background-color: var(--color-blue-200);
 	img {
 		width: 100%;
---
+++ project/client/src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+	width: 100%;
+	height: calc(100vh - 6rem);
+	display: flex;
+	flex-direction: column;
+	margin-top: 6rem;
+`

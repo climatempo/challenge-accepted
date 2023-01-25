11f8832f81d04ccf7866f7f5ebc26b6919a115a9
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-25 18:12:56 -0300

    feat: implements all the api routes

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -8,4 +8,22 @@ app.options('*', cors());
 app.use(cors());
 app.use(express.json());
 
+
+const localesRoutes = require('./Routes/locales.routes');
+const weathersRoutes = require('./Routes/weathers.routes');
+
+const jwtVerifier = require('./Middlewares/jwtVerifier');
+
+// 'Hello world' from the api
+app.get('/', (_req, res) =>
+  res.status(200).json({ message: 'Climatempo says hello :)' })
+);
+
+// all the requests below needs a JWT 'authorization' headers key
+// example: { headers: { authorization: secret_example } }
+app.use(jwtVerifier);
+
+app.use('/locales', localesRoutes);
+app.use('/weathers', weathersRoutes);
+
 module.exports = app;

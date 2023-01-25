829f98be8155224edddefa3200d3f429f8cfb7a1
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-25 18:01:44 -0300

    feat:  strings formatter for having a bigger amount of right results

---
+++ app/backend/src/utils/formatLocalesName.js
@@ -0,0 +1,11 @@
+const removeAccents = require("./removeAccents");
+
+// this function allows the application get more results if someone write any locale with a wrong name, wrong spaces, or even wrong accents, getting a bigger amount of right results
+
+const formatLocalesName = (localeName) => {
+  const formatedLocaleName = removeAccents(localeName.toLowerCase());
+  const formatedLocaleNameWithoutSpaces = formatedLocaleName.replace(/\s/g, '')
+  return formatedLocaleNameWithoutSpaces;
+};
+
+module.exports = formatLocalesName;

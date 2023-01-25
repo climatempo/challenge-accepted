ef078233e14a85fc4d0ff9213a38870a25296afa
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-25 17:59:18 -0300

    feat: `getLocaleByName` method for LocalesServices

--- app/backend/src/Services/localesServices.js
+++ app/backend/src/Services/localesServices.js
@@ -11,6 +11,17 @@ class LocalesServices {
     const allLocales = await this._localesModel();
     return allLocales;
   };
+
+  getLocaleByName = async (localeName) => {
+    const allLocales = await this._localesModel();
+
+    // since it will have only one locale by name, it's better to use find than filter or other HOF
+    const localeByName = allLocales.find(
+      (locale) => formatLocalesName(locale.name) === formatLocalesName(localeName)
+    );
+
+    return localeByName;
+  };
 }
 
 module.exports = LocalesServices;

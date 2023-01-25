4c7da248609ea698d7f87efb9b6478f94b04fb9b
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-25 20:26:07 -0300

    feat: changes files dirs

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -2,8 +2,13 @@ function App() {
   return (
     <div className="">
       {/* logo */}
-      <header className="bg-primary-blue">
-        <img src="" alt="climatempo logo" />
+      <header className="h-36 flex flex-col justify-center items-center bg-primary-blue">
+        <img
+          className='transition-all hover:opacity-80'
+          src="https://imgur.com/8jlWHWD.png"
+          alt="climatempo logo"
+          width={230}
+        />
       </header>
     </div>
   );

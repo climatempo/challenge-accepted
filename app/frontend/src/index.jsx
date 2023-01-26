a43019697053df4f901f2335f0408d86ecb9e853
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-26 15:27:33 -0300

    feat: provide the context to all the app components

--- app/frontend/src/index.jsx
+++ app/frontend/src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ContextProvider from './Context/ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
 

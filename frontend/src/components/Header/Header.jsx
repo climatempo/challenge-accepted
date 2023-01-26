07a416ed736da04c470dd531bc71fd2865469983
michael <michaeledavi12@gmail.com>
2023-01-26 11:28:22 -0300

    feat: created the Header.jsx and the Header.css

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import LogoClima from "../images/logo-white.png";
+import "./Header.css";
+import { RiSettings3Fill } from 'react-icons/ri';
+import { FiX } from 'react-icons/fi';
+import UnitSelection from "../UnitSelection/UnitSelection";
+
+
+
+export default function Header({ setTemperature, setPrecipitation}) {
+    const [openPopover, setOpenPopover] = React.useState(false);
+
+    return (
+        <header className="Header">
+            <img className="Image" src={LogoClima} alt="logo climatempo" />
+            <div className="containerSettings">
+                <button className="buttonSettings" onClick={() => setOpenPopover(!openPopover)}>
+                    <RiSettings3Fill />
+                </button>
+                {openPopover && (
+                    <div className='popoverSettings'>
+                        <div className="headerPopover">
+                            <button className="buttonClose" onClick={() => setOpenPopover(false)}>
+                                <FiX />
+                            </button>
+                        </div>
+        
+                        <div>
+                            <div className='units'>
+                                <span>Temperatura:</span>
+                                <UnitSelection options={['C', 'F']} onSelect={setTemperature} />
+                            </div>
+                            <div className='units'>
+                                <span>Precipitação:</span>
+                                <UnitSelection options={['mm', 'inch']} onSelect={setPrecipitation} />
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </header>
+    );
+};

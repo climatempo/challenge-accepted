4f42e5999078da3b98335f017a9a5dd7c68173e2
michael <michaeledavi12@gmail.com>
2023-01-26 15:06:07 -0300

    fixed the loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,43 +18,70 @@ export default function App() {
   const [precipitation, setPrecipitation] = useState('mm')
   const [optionCityId, setOptionCityId] = useState(0)
   const [optionCityName, setOptionCityName] = useState('')
+  const [loading, setLoading] = useState(false)
 
 
   const citySelected = (option) => {
     if (!option) return;
+    setLoading(true);
     api.get(`/forecast?id=${option.id}&temperatureUnit=${temperature}&precipitationUnit=${precipitation}`)
       .then((response) => {
-        setForecastCity(response.data)
-        setCity(option)
+        setForecastCity(response.data);
+        setCity(option);
+        setLoading(false);
+      }).catch((error) => {
+        console.log(error)
+        setLoading(false);
       })
   }
 
   const getForecastByCityName = (cityName) => {
     if (!cityName) return;
+    setLoading(true);
     api.get(`/forecast?cityName=${cityName}&temperatureUnit=${temperature}&precipitationUnit=${precipitation}`)
       .then((response) => {
         setForecastCity(response.data)
         setCity(response.data.locale)
+        setLoading(false);
+      }).catch((error) => {
+        console.log(error)
+        setLoading(false);
       })
   }
 
   useEffect(() => {
     if (optionCityId === 0) return;
-
+   
     const fetchForecast = async () => {
-      const response = await api.get(`/forecast?id=${optionCityId}&temperatureUnit=${temperature}&precipitationUnit=${precipitation}`)
-      setForecastCity(response.data)
-      setCity(response.data.locale)
+      setLoading(true);
+      try {
+        const response = await api.get(`/forecast?id=${optionCityId}&temperatureUnit=${temperature}&precipitationUnit=${precipitation}`)
+        setForecastCity(response.data);
+        setCity(response.data.locale);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
     }
+
     fetchForecast();
+
   }, [temperature])
 
   useEffect(() => {
     if (optionCityName === '') return;
     const fetchForecast = async () => {
+      setLoading(true);
+      try {
       const response = await api.get(`/forecast?cityName=${optionCityName}&temperatureUnit=${temperature}&precipitationUnit=${precipitation}`)
-      setForecastCity(response.data)
-      setCity(response.data.locale)
+      setForecastCity(response.data);
+      setCity(response.data.locale);
+      setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
     }
     fetchForecast();
   }, [precipitation])
@@ -66,25 +93,28 @@ export default function App() {
   return (
     <div className='App'>
       <Header setPrecipitation={setPrecipitation} setTemperature={setTemperature} />
+
       <Search onSelect={citySelected} setOptionCityId={setOptionCityId} setOptionCityName={setOptionCityName} onClickSearchButton={getForecastByCityName} />
+
+      {loading && <Loading />}
+
       <NameCity city={city} clearSearch={handleClearSearch} />
-      
-      {forecastCity.weather &&
-        forecastCity.weather.map((data, index) => {
-          return (
-            <Card
-              date={dayjs(data.date).format('DD/MM/YYYY')}
-              temMax={data.temperature.max}
-              temMin={data.temperature.min}
-              precipitation={data.rain.precipitation}
-              probability={data.rain.probability}
-              temUnit={forecastCity.units.temperature}
-              precUnit={forecastCity.units.precipitation}
-              txt={data.text}
-              key={index}
-            />
-          )
-        })
+
+      {forecastCity.weather && !loading && forecastCity.weather.map((data, index) => {
+        return (
+          <Card
+            date={dayjs(data.date).format('DD/MM/YYYY')}
+            temMax={data.temperature.max}
+            temMin={data.temperature.min}
+            precipitation={data.rain.precipitation}
+            probability={data.rain.probability}
+            temUnit={forecastCity.units.temperature}
+            precUnit={forecastCity.units.precipitation}
+            txt={data.text}
+            key={index}
+          />
+        )
+      })
       }
     </div>
   )
--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,11 +1,12 @@
-import "./Loading.css";
+import React from "react";
+import './styles.css'
 
 function Loading({ height }) {
   return (
     <div className="containerLoading" style={{ height: height }}>
-         <div className="loader"></div>
+      <div className="loader"></div>
     </div>
-      );
+  );
 }
 
 export default Loading;

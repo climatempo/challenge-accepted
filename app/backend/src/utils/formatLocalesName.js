1959fffa4339b42c96fc6e79e434b7552922d52e
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-26 19:38:56 -0300

    .

--- app/backend/src/utils/formatLocalesName.js
+++ app/backend/src/utils/formatLocalesName.js
@@ -1,10 +1,10 @@
-const removeAccents = require("./removeAccents");
+const removeAccents = require('./removeAccents');
 
 // this function allows the application get more results if someone write any locale with a wrong name, wrong spaces, or even wrong accents, getting a bigger amount of right results
 
 const formatLocalesName = (localeName) => {
   const formatedLocaleName = removeAccents(localeName.toLowerCase());
-  const formatedLocaleNameWithoutSpaces = formatedLocaleName.replace(/\s/g, '')
+  const formatedLocaleNameWithoutSpaces = formatedLocaleName.replace(/\s/g, '');
   return formatedLocaleNameWithoutSpaces;
 };
 

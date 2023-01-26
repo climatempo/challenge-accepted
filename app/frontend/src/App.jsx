70ef46393e51d98b1a50d3688c0f68f23fbece92
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-26 19:42:34 -0300

    feat: implements all the components

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,16 +1,33 @@
+import { useState, useContext } from 'react';
+
+import context from './Context/context';
+
+import SwitchButton from './Components/SwitchButton';
+import WeatherCard from './Components/WeatherCard';
+
 function App() {
+  const [toggle, setToggle] = useState('C');
+
+  const { hasSearch, weathers } = useContext(context);
   return (
-    <div className="">
-      {/* logo */}
-      <header className="h-36 flex flex-col justify-center items-center bg-primary-blue">
-        <img
-          className='transition-all hover:opacity-80'
-          src="https://imgur.com/8jlWHWD.png"
-          alt="climatempo logo"
-          width={230}
-        />
-      </header>
-    </div>
+    <main>
+      <div className="flex mt-2 justify-center gap-1.5 items-center">
+        <span className="text-2xl">C °</span>
+        <SwitchButton toggle={toggle} setToggle={setToggle} />
+        <span className="text-2xl">F °</span>
+      </div>
+      {!hasSearch ? (
+        <>
+          <p className="mt-2 mb-2 w-full text-2xl text-center">
+            {typeof weathers === 'number'
+              ? "Desculpe, não encontrei essa cidade :( , você digitou o nome dela corretamente? "
+              : 'Pesquise pela temperatura da sua cidade :)'}
+          </p>
+        </>
+      ) : (
+        <WeatherCard toggle={toggle} />
+      )}
+    </main>
   );
 }
 

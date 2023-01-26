0cd710071ef4bb71455efbfdccba702a9f15841c
michael <michaeledavi12@gmail.com>
2023-01-26 11:36:07 -0300

    feat: created the UnitSelection.jsx and the UnitSelection.css

---
+++ frontend/src/components/UnitSelection/UnitSelection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./UnitSelection.css"
+
+export default function UnitSelection(props) {
+    return(
+        <select className="Selection" onChange={(event) => props.onSelect(event.target.value)}>
+            {props.options && props.options.map((option, index) => {
+                return(
+                    <option value={option} key={index}>{option}</option>
+                )
+            })}
+        </select>
+        
+    )
+}
